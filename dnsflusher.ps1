a8913bc374c6b52a45ae0006c01b6e5b412226d0
TheDoggyBrad <94173621+thedoggybrad@users.noreply.github.com>
2023-03-19 10:27:20 +0800

    Add files via upload

---
+++ dnsflusher.ps1
@@ -0,0 +1,8 @@
+﻿$Title = "TheDoggyBrad's DNS Cache Flusher for Windows"
+$host.UI.RawUI.WindowTitle = $Title
+Write-Host "Do you want to flush your dns cache?"
+pause
+ipconfig /flushdns
+Write-Host "Press Enter key to exit"
+pause
+exit
