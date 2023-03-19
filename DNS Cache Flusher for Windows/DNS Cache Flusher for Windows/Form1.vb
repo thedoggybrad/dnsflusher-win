cb9bdb329ba794fa81f39da37f07830ba5032ae4
TheDoggyBrad <94173621+thedoggybrad@users.noreply.github.com>
2023-03-19 09:55:21 +0800

    Add Visual Basic Based Application

---
+++ DNS Cache Flusher for Windows/DNS Cache Flusher for Windows/Form1.vb
@@ -0,0 +1,10 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim command = "ipconfig /flushdns" 'DNS FLUSHING COMMAND"
+        Shell(command)
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Close()
+    End Sub
+End Class
